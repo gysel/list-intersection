4508bed768dd4890cfd0c919d6d1d9e216877707
Michi Gysel <mail@mgysel.ch>
2018-04-12 19:38:51 +0200

    use constructor based test approach

--- src/test/kotlin/ch/mgysel/lists/IntersectionTest.kt
+++ src/test/kotlin/ch/mgysel/lists/IntersectionTest.kt
@@ -3,32 +3,29 @@ package ch.mgysel.lists
 import io.kotlintest.matchers.*
 import io.kotlintest.specs.StringSpec
 
-class IntersectionTest : StringSpec() {
+class IntersectionTest : StringSpec({
 
-    private val intersection = Intersection<Int>()
+    val intersection = Intersection<Int>()
 
-    init {
+    val implementations = Intersection.Implementation.values()
 
-        val implementations = Intersection.Implementation.values()
+    val firstList = listOf(2, 3, 1)
+    val secondList = listOf(4, 3, 5)
+    implementations.forEach { implementation ->
+        val function = intersection.getImplementation(implementation)
 
-        val firstList = listOf(2, 3, 1)
-        val secondList = listOf(4, 3, 5)
-        implementations.forEach { implementation ->
-            val function = intersection.getImplementation(implementation)
+        "$implementation - simple intersection" {
+            val result = function(firstList, secondList)
+            result shouldBe singleElement(3)
+        }
 
-            "$implementation - simple intersection" {
-                val result = function(firstList, secondList)
-                result shouldBe singleElement(3)
-            }
-
-            "$implementation - duplicates are shown once in result" {
-                val result = function(listOf(1, 2, 2, 3), listOf(2, 2, 3, 3))
-                result should haveSize(2)
-                result should containsAll(2, 3)
-            }
-
+        "$implementation - duplicates are shown once in result" {
+            val result = function(listOf(1, 2, 2, 3), listOf(2, 2, 3, 3))
+            result should haveSize(2)
+            result should containsAll(2, 3)
         }
 
     }
 
-}
+})
+
