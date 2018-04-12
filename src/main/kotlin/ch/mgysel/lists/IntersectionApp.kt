431761c71bb1ed4163e425eb66af77227665b904
Michi Gysel <mail@mgysel.ch>
2018-04-12 19:43:55 +0200

    moved main function to app class

--- src/main/kotlin/ch/mgysel/lists/IntersectionApp.kt
+++ src/main/kotlin/ch/mgysel/lists/IntersectionApp.kt
@@ -2,7 +2,17 @@ package ch.mgysel.lists
 
 import ch.mgysel.lists.css.Styles
 import ch.mgysel.lists.view.IntersectionView
+import javafx.application.Application
 import javafx.scene.image.Image
 import tornadofx.*
 
 class IntersectionApp : App(Image("icon.png"), IntersectionView::class, Styles::class)
+
+fun main(args: Array<String>) {
+    // JavaFX uses java.util.logging, redirect that to Log4J
+    System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager")
+
+    Application.launch(IntersectionApp::class.java, *args)
+
+}
+
