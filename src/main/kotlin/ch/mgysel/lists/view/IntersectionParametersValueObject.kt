810f23fd96722cb0ea0fcd8b061127582390b9eb
Michi Gysel <mail@mgysel.ch>
2018-04-13 07:14:42 +0200

    removed unused file

--- src/main/kotlin/ch/mgysel/lists/view/IntersectionParametersValueObject.kt
+++
@@ -1,8 +0,0 @@
-package ch.mgysel.lists.view
-
-/**
- * A [Value Object](https://martinfowler.com/bliki/ValueObject.html) according to Martin Fowler.
- */
-data class IntersectionParametersValueObject(val sizeA: Int,
-                                             val sizeB: Int,
-                                             val repetitions: Int)
