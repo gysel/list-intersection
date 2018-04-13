7b88203c29383cbcee48d2b6db25c29c8666e466
Michi Gysel <mail@mgysel.ch>
2018-04-13 08:35:12 +0200

    intersection now returns a set

--- src/main/kotlin/ch/mgysel/lists/Intersection.kt
+++ src/main/kotlin/ch/mgysel/lists/Intersection.kt
@@ -2,23 +2,22 @@ package ch.mgysel.lists
 
 enum class Intersection {
     KOTLIN_STDLIB {
-        override fun <T> apply(first: Collection<T>, second: Collection<T>): Collection<T> {
+        override fun <T> apply(first: Collection<T>, second: Collection<T>): Set<T> {
             return first intersect second
         }
     },
     ITERATION {
-        override fun <T> apply(first: Collection<T>, second: Collection<T>): Collection<T> {
+        override fun <T> apply(first: Collection<T>, second: Collection<T>): Set<T> {
             val set = first.toSet()
             return second.filter { set.contains(it) }.toSet()
         }
     },
     SEQUENCE {
-        override fun <T> apply(first: Collection<T>, second: Collection<T>): Collection<T> {
+        override fun <T> apply(first: Collection<T>, second: Collection<T>): Set<T> {
             val set = first.toSet()
             return second.asSequence().filter { set.contains(it) }.toSet()
         }
     };
 
-    abstract fun <T> apply(first: Collection<T>, second: Collection<T>): Collection<T>
+    abstract fun <T> apply(first: Collection<T>, second: Collection<T>): Set<T>
 }
-
